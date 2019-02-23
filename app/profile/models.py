f5bfad8050b016a874e86b84e7552a2c03421996
Ronan <ruth_ronan@yahoo.ie>
2019-02-23 21:12:11 +0000

    Created models for extended researcher profile

--- app/profile/models.py
+++ app/profile/models.py
@@ -46,8 +46,9 @@ class Admin(db.Model):
 
 class Education(db.Model):
 
-    __tablename__ = "education"
-
+    __tablename__ = "educations"
+    
+    researcher = db.relationship("Researcher", backref="education")
     researcher_id = db.Column(db.Integer, db.ForeignKey("researchers.user_id"),
                                                        primary_key=True)
     degree = db.Column(db.String, nullable=True)
@@ -55,6 +56,178 @@ class Education(db.Model):
     institution = db.Column(db.String, nullable=True)
     location = db.Column(db.String, nullable=True)
     degree_award_year = db.Column(db.String, nullable=True)
+
+class Employment(db.Model):
+
+    __tablename__ = "employments"
+    
+    researcher = db.relationship("Researcher", backref="education")
+    researcher_id = db.Column(db.Integer, db.ForeignKey("researchers.user_id"),
+                                                       primary_key=True)
+    institution = XX
+    location = XX
+    years = AA
+    
+class Membership(db.Model):
+    """The same as professional societies"""
+
+    __tablename__ = "memberships"
+
+    start_date  = db.Column(db.DateTime)
+    end_date = db.Column(db.DateTime)
+    society_name = XX
+    membership_type = XX
+    status = db.Column(db.Boolean)
+
+class Award(db.Model):
+
+    __tablename__ = "awards"
+
+    year = db.Column(db.Integer)
+    awarding_body = db.Column(db.String)
+    details = db.Column(db.String)
+    team_member_name = db.Column(db.String)
+
+class FundingDiversification(db.Model):
+
+    __tablename__ = "funding_diversification"
+
+    start_date = db.Column(db.DateTime)
+    end_date = db.Column(db.DateTime)
+    amount = db.Column(db.Integer)
+    funding_body = db.Column(db.String)
+    funding_programme = db.Column(db.String)
+    status = db.Column(db.Boolean)
+    primary_attribution = db.Column(db.Integer, db.ForeignKey("calls.id"))
+
+class TeamMember(db.Model):
+
+    __tablename__ = "team_member"
+
+    start_date = db.Column(db.DateTime)
+    departure_date = db.Column(db.DateTime)
+    name = db.Column(db.String)
+    position = db.Column(db.String)
+    primary_attribution = db.Column(db.Integer, db.ForeignKey("calls.id"))
+
+class Impact(db.Model):
+
+    __tablename__ = "impacts"
+
+    title = db.Column(db.String)
+    category = db.Column(db.String)
+    primary_benneficiary = db.Column(db.String)
+    primary_attribution = db.Column(db.Integer, db.ForeignKey("calls.id"))
+
+class Publication(db.Model):
+
+    __tablename__ = "publications"
+    
+    year = db.Column(db.Integer)
+
+    original_article = XX
+    review_article = XX
+    conference_paper = XX
+    book = XX
+    technical_report = XX
+
+    title = XX
+    journal_name = XX
+
+    is_published = db.Column(db.Boolean)
+    in_press = db.Column(db.Boolean)
+    DOI = XX
+    primary_attribution = db.Column(db.Integer, db.ForeigmKey("calls.id"))
+
+class Presentation(db.Model):
+
+    __tablename__ = "presentations"
+
+    year = db.Column(db.Integer)
+    title = XX
+
+    conference = XX
+    invited_seminar = XX
+    keynote = XX
+    organizing_body = XX
+    location = XX
+    primary_attribution = db.Column(db.Integer, db.ForeignKey("calls.id"))
+
+class AcademicCollaboration(db.Model):
+
+    __tablename__ - "academic_collaborations"
+
+    start_date = db.Column(db.DateTime)
+    end_date = db.Column(db.DateTime)
+    institution = XX
+    institution_dept = XX
+    location = XX
+    collaborator_name = XX
+    primary_goal = XX
+    interaction_frequency = XX
+    primary_attribution = db.Column(db.Integer, db.ForeignKey("calls.id"))
+
+class NonAcademicCollaboration(db.Model):
+
+    __tablename__ = "non_academic_collaborations"
+
+    start_date = db.Column(db.DateTime)
+    end_date = db.Column(db.DateTime)
+    institution = XX
+    institution_dept = XX
+    location = XX
+    collaborator_name = XX
+    primary_goal = XX
+    interaction_frequency = XX
+    primary_attribution = db.Column(db.Integer, db.ForeignKey("calls.id"))
+
+class Conference(db.Model):
+
+    __tablename__ = "conferences"
+
+    start_date = db.Column(db.DateTime)
+    end_date = db.Column(db.DateTime)
+    title = XX
+    event_type = XX
+    role = XX
+    location = XX
+    primary_attribution = db.Column(db.Integer, db.ForeignKey("calls.id"))
+
+class CommunicationOverview(db.Model):
+    
+    __tablename__ = "communication_overview"
+
+    year = db.Column(db.Integer)
+    num_of_publication_lectures = db.Column(db.Integer)
+    num_of_visits = db.Column(db.Integer)
+    num_of_media_interactions = db.Column(db.Integer)
+
+class SFIFundingRatio(db.Model):
+
+    __tablename__ = "sfi_funding_ratios"
+
+    year = db.Column(db.Integer)
+    percentage_of_annual_spend = db.Column(db.Integer)
+
+class EducationAndPublicEngagement(db.Model):
+
+    __tablename__ = "education_and_public_engagement"
+
+    project_name = db.Column(db.String)
+    start_date = db.Column(db.DateTime)
+    end_date = db.Column(db.DateTime)
+    activity_type = XX
+    project_topic = XX
+    target_graphical_area = XX
+    primary_attribution = db.Column(db.Integer, db.ForeignKey("calls.id"))
+
+
+
+
+
+
+
+
 
 class TeamMembers(db.Model):
 
