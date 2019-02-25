6c830804a82206b260b5e548f426696996d04d5c
Superrome77 <stephenoconnell77@gmail.com>
2019-02-25 16:40:23 +0000

    Add files via upload
    Added object references for all fields on the profile page

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -4,15 +4,16 @@ from flask import Flask, render_template, flash, redirect, url_for
 # Import the extensions used here
 from app import db, bcrypt, login_manager, mail
 from app.auth.forms import LoginForm, RegistrationForm, TeamForm
-from flask_login import UserMixin, current_user, login_user, logout_user
+from flask_login import UserMixin, current_user, login_user, logout_user, login_required
 from flask_mail import Message
 
 # Import auth blueprint
 from app.auth import auth
 
 # Import the Models used
-from app.profile.models import Researcher, Teams, TeamMembers
-from app.profile.models import User
+
+from app.profile.models import *
+
 
 
 @login_manager.user_loader
@@ -60,7 +61,8 @@ def register():
 @auth.route("/login", methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
-        return "you are logged in"#redirect(url_for(''))
+        # return "you are logged in"
+        redirect(url_for('auth.home'))
     form = LoginForm()
     if form.validate_on_submit():
         #Checks email instead of username
@@ -69,7 +71,7 @@ def login():
             #If we decide to implement a remember me function
             login_user(user)#, remember=form.remember.data)
             flash("You are now logged in")
-            return redirect(url_for('auth.login'))
+            return redirect(url_for('auth.home'))
         else:
             flash('Login Unsuccessful. Please check e-mail and password')
     return render_template('auth/login.html',title='Login', form=form)
@@ -84,6 +86,7 @@ def query():
     users = User.query.all()
     return str(len(users))
 
+
 @auth.route("/call-for-proposals", methods=['GET', 'POST'])
 def call_for_proposals():
     form = CallForProposalsForm()
@@ -97,16 +100,90 @@ def call_for_proposals():
 
     return render_template("auth/proposals.html", title="Call For Proposals", form=form)
 
-@auth.route("/teams")
+@auth.route("/CreateNewAdmin")
+@login_required
+def CreateNewAdmin():
+    if current_user.role == "ADMIN":
+        form = RegistrationForm()
+        if form.validate_on_submit():
+            user = User.query.filter_by(email=form.email.data).first()
+            if not user:
+
+                password = bcrypt.generate_password_hash(form.password.data).decode("utf-8")
+                user = User(form.email.data, password, "ADMIN")
+                db.session.add(user)
+                db.session.commit()
+
+                user = User.query.filter_by(email=form.email.data).first()
+                admin = Admin(user_id=user.id, first_name=form.first_name.data, last_name=form.last_name.data,
+                                        job_title=form.job_title.data, prefix=form.prefix.data, suffix=form.suffix.data,
+                                        phone=form.phone.data, phone_ext=form.phone_ext.data, orcid=form.orcid.data)
+                db.session.add(admin)
+                # education = Education(user_id=user.id, degree=None, field_of_study=None, institution=None,
+                #                         location=None,degree_award_year=None)
+                # db.session.add(education)
+                db.session.commit()
+
+                flash("Admin account has been created. You can now login")
+                return redirect(url_for("auth.login"))
+            else:
+                flash("An account already exists with this email address. Please login.")
+        return render_template("auth/register.html", form=form)
+
+@auth.route("/teams", methods=['GET','POST'])
+
 def team_form():
-    form = TeamForm()
-    if form.validate_on_submit():
-        teamMem = TeamMembers(form.start_date.data,form.end_date.data,form.name.data,form.position.data,form.grant_number.data)
-        db.session.add(teamMem)
-        db.session.commit()
-        flash("Your team member has been added!")
-    members = TeamMembers.query.all()
-    #teams = Teams.query.all()
-    #for team in teams:
-        #if team[1] == researcher's primary attribute
-    return render_template('auth/team_form.html',title="Enter Team", form=form, members=members) #
+    if current_user.is_authenticated:
+        form = TeamForm()
+        researcher_id = current_user.id
+        if form.validate_on_submit():
+            teamMem = TeamMembers(start_date = form.start_date.data, end_date = form.end_date.data, name = form.name.data,position = form.position.data, primary_attribute = form.grant_number.data,researcher_id = researcher_id)
+            db.session.add(teamMem)
+            db.session.commit()
+            flash("Your team member has been added!") 
+        members = TeamMembers.query.filter_by(researcher_id = researcher_id).all()
+        for member in members:
+            print(member.name)
+        return render_template('auth/team_form.html',title="Enter Team", form=form, members=members)
+    else:
+        return redirect(url_for("auth.login"))
+
+
+@auth.route("/profile",methods=['GET'])
+@login_required
+def profile(): 
+    user = Researcher.query.filter_by(user_id=current_user.id).first()
+    user_education = Education.query.filter_by(researcher_id=current_user.id).first()
+    user_employment = Employment.query.filter_by(researcher_id=current_user.id).first()
+    user_membership = Membership.query.filter_by(researcher_id=current_user.id).first()
+    user_award = Award.query.filter_by(researcher_id=current_user.id).first()
+    user_funding = FundingDiversification.query.filter_by(researcher_id=current_user.id).first()
+    user_team = TeamMember.query.filter_by(researcher_id=current_user.id).first()
+    user_impact = Impact.query.filter_by(researcher_id=current_user.id).first()
+    user_innovation = Innovation.query.filter_by(researcher_id=current_user.id).first()
+    user_publication = Publication.query.filter_by(researcher_id=current_user.id).first()
+    user_presentation = Presentation.query.filter_by(researcher_id=current_user.id).first()
+    user_academicCollabaration = AcademicCollabaration.query.filter_by(researcher_id=current_user.id).first()
+    user_nonAcademicCollabaration = NonAcademicCollabaration.query.filter_by(researcher_id=current_user.id).first()
+    user_confrence = Confrence.query.filter_by(researcher_id=current_user.id).first()
+    user_communicationOverview = CommunicationOverview.query.filter_by(researcher_id=current_user.id).first()
+    user_sfiFunding = SFIFundingRatio.query.filter_by(researcher_id=current_user.id).first()
+    user_educationAndPublicEngagement = EducaionAndPublicEngagement.query.filter_by(researcher_id=current_user.id).first()
+    return render_template('auth/account.html', title ="Profile",user=user,user_education=user_education,user_employment=user_employment,user_membership=user_membership,user_award=user_award,user_funding=user_funding,user_team=user_team,user_impact=user_impact,user_innovation=user_innovation,user_publication=user_publication,user_presentation=user_presentation,user_academicCollabaration=user_academicCollabaration,user_nonAcademicCollabaration=user_nonAcademicCollabaration,user_confrence=user_confrence,user_communicationOverview=user_communicationOverview,user_sfiFundingq=user_sfiFunding,user_educationAndPublicEngagement=user_educationAndPublicEngagement)
+
+
+#@auth.route("/stats",methods=['GET'])
+#@login_required
+#def 
+
+
+
+
+@auth.route("/createAdmin",methods=["GET","POST"])
+def createAdmin():
+    password = bcrypt.generate_password_hash("password").decode("utf-8")
+    user = User("admin",password, "ADMIN")
+    db.session.add(user)
+    db.session.commit()
+    flash("Admin made")
+    return redirect(url_for("auth.login"))
