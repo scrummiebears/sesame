1ba008a777968fba4e4afd013153a4b8ade1e33c
Ronan <ruth_ronan@yahoo.ie>
2019-02-28 22:20:30 +0000

    migratedd route for creating a new admin

---
+++ app/admin/forms.py
@@ -0,0 +1,10 @@
+from flask_wtf import FlaskForm
+from wtforms.fields import StringField, PasswordField
+
+class NewAdminForm(FlaskForm):
+
+    email = StringField("Email")
+    password = PasswordField("Password")
+
+    first_name = StringField("First Name")
+    last_name = StringField("Last name")
--- app/admin/routes.py
+++ app/admin/routes.py
@@ -1,17 +1,44 @@
+from app.admin import admin
+from flask import render_template, url_for, redirect
+from app.call_system.models import *
+
 @admin.route("dashboard")
 def dashboard():
-    return render_template("admin/dashboard.html", user=admin)
+    admin = None#current_user.admin
+    p_pending_admin_1 = len(Proposal.query.filter(Proposal.status == "PENDING ADMIN 1").all()) or 0
+    p_pending_review = len(Proposal.query.filter(Proposal.status == "PENDING REVIEWER").all())
+    p_pending_admin_2 = len(Proposal.query.filter(Proposal.status == "PENDING ADMIN 2").all())
+    p_approved = len(Proposal.query.filter(Proposal.status == "APPROVED").all())
+    p_stats = {"PENDING ADMIN 1": p_pending_admin_1, "PENDING ADMIN 2": p_pending_admin_2, "PENDING REVIEW": p_pending_review, "APPROVED":p_approved}
+    return render_template("admin/dashboard.html", user=admin, proposal_stats=p_stats)
 
-@admin.route("new_admin")
+@admin.route("new_admin", methodfisrt_names=["GET", "POST"])
 def newAdmin():
-    return render_template("admin/new_admin.html", user=admin)
+    form = NewAdminForm
+    if request.method == "POST" and form.validate:
+
+        password = bcrypt.generate_password_hash(form.password.data).decode("utf-8")
+        user = User(form.email.data, password, "ADMIN")
+        db.session.add(user)
+        db.session.commit()
+
+        user = User.query.filter_by(email=form.email.data).first()
+
+        new_admin = Admin(user_id=user.id, first_name=form.fisrt_name.data, last_name=form.last_name.data)
+        db.session.add(new_admin)
+        db.session.commit()
+        flash("New admin created - %s %s" % (form.first_name.data, form.last_name.data))
+        return redirect(url_for(admin.newAdmin))
+    admin = current_user.admin
+    return render_template("admin/new_admin.html", user=admin, form=form)
 
 @admin.route("<call>/proposals")
 def proposals(call):
     return render_template("admin/proposals.html")
 
 @admin.route("<proposal>/review")
-def review(proposal)
+def review(proposal):
+    return render_template("admin/review.html")
 
 @admin.route("<proposal>/deny")
 def rejectProposal(propsoal):
