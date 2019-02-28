2da00cf963fa11b4638683696e82c6f0da78507d
Ronan <ruth_ronan@yahoo.ie>
2019-02-28 23:46:03 +0000

    moved admin data model from profile to admin

--- app/profile/models.py
+++ app/profile/models.py
@@ -6,16 +6,6 @@ from app.auth.models import User
 
 from datetime import date
 
-class Admin(db.Model):
-    
-    __tablename__ = "admins"
-
-    user = db.relationship("User", backref=db.backref("admin", uselist=False))
-    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), primary_key=True)
-
-    first_name = db.Column(db.String)
-    last_name = db.Column(db.String)
-
 class Researcher(db.Model):
 
     __tablename__ = "researchers"
